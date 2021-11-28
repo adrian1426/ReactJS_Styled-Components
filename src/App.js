8e25ba45ed0afe9bd590271fbc8ad37e27f64ae0
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2021-11-27 18:00:25 -0600

    adaptando componentes en base a props

--- src/App.js
+++ src/App.js
@@ -9,10 +9,20 @@ const Content = styled.div`
   padding: 20px 25px;
 `;
 
+const Button = styled.button`
+  background-color:${props => props.primary ? 'red' : 'white'};
+  color: ${props => props.primary ? 'white' : 'red'};
+  padding: 10px 15px;
+  border: solid 2px red;
+  border-radius:4px;
+`;
+
+
 function App() {
   return (
     <Content>
       <P>Hola soy un parrafo!</P>
+      <Button primary>Enviar</Button>
     </Content>
   );
 }
