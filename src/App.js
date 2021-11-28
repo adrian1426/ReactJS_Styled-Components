85fb359ec84294283868d0a4907cdc17722be646
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2021-11-27 18:18:54 -0600

    extendiendo estilos

--- src/App.js
+++ src/App.js
@@ -17,12 +17,17 @@ const Button = styled.button`
   border-radius:4px;
 `;
 
+const BlockButton = styled(Button)`
+  width:100%;
+  font-size:24px;
+`;
 
 function App() {
   return (
     <Content>
       <P>Hola soy un parrafo!</P>
       <Button primary>Enviar</Button>
+      <BlockButton primary>Enviar</BlockButton>
     </Content>
   );
 }
